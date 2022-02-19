ca13d89b48725ab23b76d6e389c3ddc636176f12
Lulky <lukyluxiano.12@gmail.com>
2022-02-19 15:26:49 -0500

    Bucles

---
+++ BuclesDesafio.js
@@ -0,0 +1,33 @@
+
+// DESAFIO 1
+// // for(i=0; i<20; i++){
+// //     i+=1;
+// //     console.log(i);
+// // }
+
+// DESAFIO 2
+// // for(i=100; i>=0; i--) {
+// //     if(i % 3 == 0) {
+// //         console.log(i)
+// //     }
+// // }
+
+// DESAFIO 3
+// // for(i=4; i>-5; i-=1.5) {
+// //     console.log(i)
+// // }
+
+// DESAFIO 4
+// // var sum =0
+// //     for(i=0; i<=100; i++){
+// //     sum+=i
+// // }
+// // console.log(sum)
+
+// DESAFIO 5
+
+var product=1
+    for(i=1; i<=12; i++) {
+        product=product*i
+    }
+    console.log(product)
